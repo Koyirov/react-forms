a5787340818dfaad5b39fd29852d0f61ea83de8c
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-03-29 12:57:51 +0100

    validating input via built-in validation props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header.jsx';
-// import Login from './components/Login.jsx';
+import Login from './components/Login.jsx';
 // import Signup from './components/Signup.jsx';
 import StateLogin from './components/StateLogin.jsx';
 
@@ -8,9 +8,9 @@ function App() {
     <>
       <Header />
       <main>
-        {/*<Login />*/}
+        <Login />
         {/*<Signup />*/}
-        <StateLogin />
+        {/*<StateLogin />*/}
       </main>
     </>
   );
