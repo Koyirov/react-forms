715887a490ca40ba41e07aa74b3384668b3df106
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2025-03-29 10:15:14 +0100

    prettier config added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,45 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Submitted!');
+  };
+
+  const handleEmailChange = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+          />
         </div>
       </div>
 
